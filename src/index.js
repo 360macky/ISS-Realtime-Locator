43c00f517fdbb8b6eee92016709fa445e5ea8bd6
Marcelo Arias <hola@marceloarias.com>
2021-12-05 18:11:26 -0500

    Move container React root

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import './styles/sass/App.scss';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById('root');
+ReactDOM.render(<App />, container);
